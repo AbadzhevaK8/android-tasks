2817df7bc76189becad7de52a59ef289d45132d9
k8abadzheva <make.me.loco@gmail.com>
2025-01-10 23:05:06 +0300

    feat: add TaskDatabase
    Added a Room database class for task persistence.
    
    - Created a TaskDatabase class extending RoomDatabase.
    - Defined Task entity as part of the database.

---
+++ app/src/main/java/com/abadzheva/tasks/TaskDatabase.kt
@@ -0,0 +1,8 @@
+package com.abadzheva.tasks
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [Task::class], version = 1, exportSchema = false)
+abstract class TaskDatabase : RoomDatabase() {
+}
