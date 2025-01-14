90287e4cbb335de2c7ec2c4b5319f6e84d20d1e6
k8abadzheva <make.me.loco@gmail.com>
2025-01-14 20:37:48 +0300

    feat: Implement TaskViewModel and update TaskDao
    - Created TaskViewModel to handle task creation logic using viewModelScope.
    - Updated TaskDao to use suspend functions for insert, update, and delete operations.

---
+++ app/src/main/java/com/abadzheva/tasks/TaskViewModel.kt
@@ -0,0 +1,19 @@
+package com.abadzheva.tasks
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+
+class TaskViewModel(
+    val dao: TaskDao,
+) : ViewModel() {
+    var newTaskName = ""
+
+    fun addTask() {
+        viewModelScope.launch {
+            val task = Task()
+            task.taskName = newTaskName
+            dao.insert(task)
+        }
+    }
+}
