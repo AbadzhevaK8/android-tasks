df75706b1a464b8db6df918da9831be3b0d8b57a
k8abadzheva <make.me.loco@gmail.com>
2025-01-20 21:35:09 +0300

    feat: implement TaskItemAdapter
    Implemented `TaskItemAdapter` to display tasks in a RecyclerView:
    
    - Created a `TaskItemViewHolder` class to hold and display task data within a TextView.
    - Implemented `inflateFrom` method to create a `TaskItemViewHolder` instance from a layout file.
    - Implemented `onBindViewHolder` to populate the ViewHolder with task data.
    - Added `onCreateViewHolder` to create a `TaskItemViewHolder` instance and return it.
    - Updated `getItemCount` to return the size of the data.
    - Created `task_item` layout file for one task item.

--- app/src/main/java/com/abadzheva/tasks/presentation/TaskItemAdapter.kt
+++ app/src/main/java/com/abadzheva/tasks/presentation/TaskItemAdapter.kt
@@ -1,8 +1,11 @@
 package com.abadzheva.tasks.presentation
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.Adapter
+import com.abadzheva.tasks.R
 import com.abadzheva.tasks.model.Task
 import com.abadzheva.tasks.presentation.TaskItemAdapter.TaskItemViewHolder
 
@@ -15,7 +18,34 @@ class TaskItemAdapter : Adapter<TaskItemViewHolder>() {
 
     override fun getItemCount() = data.size
 
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int,
+    ): TaskItemViewHolder = TaskItemViewHolder.inflateFrom(parent)
+
+    override fun onBindViewHolder(
+        holder: TaskItemViewHolder,
+        position: Int,
+    ) {
+        val item = data[position]
+        holder.bind(item)
+    }
+
     class TaskItemViewHolder(
         val rootView: TextView,
-    ) : RecyclerView.ViewHolder(rootView)
+    ) : RecyclerView.ViewHolder(rootView) {
+        fun bind(item: Task) {
+            rootView.text = item.taskName
+        }
+
+        companion object {
+            fun inflateFrom(parent: ViewGroup): TaskItemViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val view =
+                    layoutInflater
+                        .inflate(R.layout.task_item, parent, false) as TextView
+                return TaskItemViewHolder(view)
+            }
+        }
+    }
 }
