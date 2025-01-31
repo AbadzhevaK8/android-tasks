0ffcd71aef7450a823cb0567e93464a9a864130d
k8abadzheva <make.me.loco@gmail.com>
2025-01-31 18:03:25 +0300

    feat: display tasks from database in TasksFragment
    Updated TasksFragment to display tasks from the database using a `RecyclerView` and `ListAdapter`:
    
    - Created `TaskDiffItemCallback` to handle task list updates efficiently.
    - Replaced `TaskItemAdapter` with `ListAdapter` in `TasksFragment` to display tasks.
    - Added `submitList` call to the adapter to refresh displayed data from `LiveData`.
    - Initialized `TasksViewModel` by calling `get()` on `ViewModelProvider`.
    - Updated `TaskDatabase` to call `getInstance()` by using the `Companion` object.
    - Moved observers into the `onCreateView`.
    - Added `lifecycleOwner` to binding.

--- app/src/main/java/com/abadzheva/tasks/presentation/TaskDiffItemCallback.kt
+++ app/src/main/java/com/abadzheva/tasks/presentation/TaskDiffItemCallback.kt
@@ -4,18 +4,6 @@ import androidx.recyclerview.widget.DiffUtil
 import com.abadzheva.tasks.model.Task
 
 class TaskDiffItemCallback : DiffUtil.ItemCallback<Task>() {
-    override fun areItemsTheSame(
-        oldItem: Task,
-        newItem: Task
-    ): Boolean {
-        return (oldItem.taskId == newItem.taskId)
-    }
-
-    override fun areContentsTheSame(
-        oldItem: Task,
-        newItem: Task
-    ): Boolean {
-        return (oldItem == newItem)
-    }
-
+    override fun areItemsTheSame(oldItem: Task, newItem: Task) = (oldItem.taskId == newItem.taskId)
+    override fun areContentsTheSame(oldItem: Task, newItem: Task) = (oldItem == newItem)
 }
--- app/src/main/java/com/abadzheva/tasks/presentation/TasksFragment.kt
+++ app/src/main/java/com/abadzheva/tasks/presentation/TasksFragment.kt
@@ -13,41 +13,30 @@ import com.abadzheva.tasks.viewmodel.TasksViewModel
 import com.abadzheva.tasks.viewmodel.TasksViewModelFactory
 
 class TasksFragment : Fragment() {
-    @Suppress("ktlint:standard:backing-property-naming")
     private var _binding: FragmentTasksBinding? = null
     private val binding get() = _binding!!
-
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?,
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentTasksBinding.inflate(inflater, container, false)
         val view = binding.root
-
         val application = requireNotNull(this.activity).application
-        val dao = TaskDatabase.Companion.getInstance(application).taskDao
+        val dao = TaskDatabase.getInstance(application).taskDao
         val viewModelFactory = TasksViewModelFactory(dao)
-        val viewModel =
-            ViewModelProvider(
-                this,
-                viewModelFactory,
-            )[TasksViewModel::class.java]
-
+        val viewModel = ViewModelProvider(
+            this, viewModelFactory
+        ).get(TasksViewModel::class.java)
         binding.viewModel = viewModel
         binding.lifecycleOwner = viewLifecycleOwner
 
         val adapter = TaskItemAdapter()
         binding.tasksList.adapter = adapter
 
-        viewModel.tasks.observe(
-            viewLifecycleOwner,
-            Observer {
-                it?.let {
-                    adapter.submitList(it)
-                }
-            },
-        )
+        viewModel.tasks.observe(viewLifecycleOwner, Observer {
+            it?.let {
+                adapter.submitList(it)
+            }
+        })
 
         return view
     }
