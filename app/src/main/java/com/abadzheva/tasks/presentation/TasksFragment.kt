7e6d069d8c67fe0a04eb78eb21ec617e1147b26c
k8abadzheva <make.me.loco@gmail.com>
2025-02-05 22:34:04 +0300

    feat: navigate to task details on task click
    Implemented navigation from the TasksFragment to the EditTaskFragment when a task is clicked:
    
    - Added a click listener to the `TaskItemAdapter` that calls `viewModel.onTaskClicked(taskId)`.
    - Added a `navigateToTask` LiveData in the `TasksViewModel` to observe task click events.
    - Added an observer to the `navigateToTask` LiveData in the `TasksFragment`.
    - Created an action `actionTasksFragmentToEditTaskFragment` in navigation graph.
    - Upon task click, navigate to `EditTaskFragment` with the corresponding `taskId`.
    - Implemented `onTaskNavigated` in the `TasksViewModel` to reset the `navigateToTask` after navigation.

--- app/src/main/java/com/abadzheva/tasks/presentation/TasksFragment.kt
+++ app/src/main/java/com/abadzheva/tasks/presentation/TasksFragment.kt
@@ -4,10 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.abadzheva.tasks.databinding.FragmentTasksBinding
 import com.abadzheva.tasks.model.TaskDatabase
 import com.abadzheva.tasks.viewmodel.TasksViewModel
@@ -31,16 +31,21 @@ class TasksFragment : Fragment() {
         binding.lifecycleOwner = viewLifecycleOwner
 
         val adapter = TaskItemAdapter { taskId ->
-            Toast.makeText(
-                context, "Clicked task $taskId",
-                Toast.LENGTH_SHORT
-            ).show()
+            viewModel.onTaskClicked(taskId)
         }
         binding.tasksList.adapter = adapter
 
         viewModel.tasks.observe(viewLifecycleOwner, Observer {
             it?.let {
                 adapter.submitList(it)
+            }
+        })
+
+        viewModel.navigateToTask.observe(viewLifecycleOwner, Observer { taskId ->
+            taskId?.let {
+                val action = TasksFragmentDirections.actionTasksFragmentToEditTaskFragment(taskId)
+                this.findNavController().navigate(action)
+                viewModel.onTaskNavigated()
             }
         })
 
